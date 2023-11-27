657ff6dd3c2438db401ecf757d7fd66c0501ae66
ckh <gusrudchl12@gmail.com>
2023-11-27 13:36:14 +0900

    11-27

--- src/main/java/com/std/sbb/Question/QuestionController.java
+++ src/main/java/com/std/sbb/Question/QuestionController.java
@@ -6,11 +6,13 @@ import com.std.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.zip.DataFormatException;
@@ -55,5 +57,19 @@ public class QuestionController {
         Question q = this.questionService.create( questionForm.getSubject(), questionForm.getContent(), siteUser);
 
         return "redirect:/question/list";
+    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getSiteUser().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
--- src/main/java/com/std/sbb/Question/QuestionService.java
+++ src/main/java/com/std/sbb/Question/QuestionService.java
@@ -47,4 +47,10 @@ public class QuestionService {
             this.questionRepository.save(q);
             return q;
     }
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
 }
